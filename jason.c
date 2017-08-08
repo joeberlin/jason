204dee60abb23da19688978a7ef61579b9782edf
joeberlin <30549549+joeberlin@users.noreply.github.com>
2017-08-08 16:57:27 +0530

    Create jason.c

---
+++ jason.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("enter the year");
+scanf("%d" ,&year);
+if(year%300==0)
+{
+printf("enter the year is not leep year");
+}
+else if(year%200==0)
+{
+printf("enter the year is not leep year");
+}
+else
+{
+printf("enter the year is leep year");
+}
+getch();
+}
