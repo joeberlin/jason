94eb056bb2f74c651c9a8af72291678e6391616a
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-17 16:18:26 +0530

    Create ricbeni.c

---
+++ ricbeni.c
@@ -0,0 +1,23 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+{
+printf("enter the a,b,c value");
+scanf("%d,%d,%d, &a,&b,&c);
+}
+if(a>b && a>c)
+{
+printf("a the greast number");
+}
+else if(b>c && b>a)
+{
+printf(" b the greast number");
+}
+else(c>a && c>b)
+{
+printf(" c the greast number");
+}
+getch();
+}
