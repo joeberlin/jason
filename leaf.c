680d4bc64f0a9b4ed6c99c94dfc66222954832f5
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-17 16:32:16 +0530

    Create leaf.c

---
+++ leaf.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,year;
+{
+printf("enter the year");
+scanf("%d",&year);
+{
+if(4==0)
+{
+printf("this year is leaf year");
+}
+if(100==0)
+{
+printf("this year is leaf year);
+}
+if(400==0)
+{
+printf("this year is leaf year");
+}
+else
+{
+printf("this year is not leaf year);
+}
+else
+{
+printf("thisyear is not leaf year");
+}
+else("this year is not leaf year");
+}
+}
+getch();
+}
