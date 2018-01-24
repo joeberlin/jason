454ac0bb7d0a9be674f21af3b1741bf4093d2989
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-24 16:04:15 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ #include<conio.h.
+ void main
+ {
+ int k,n;
+ {
+ printf("How many times you want to display hello world?\n");
+ scanf("%d", &n);
+ }
+ for (k=1; k<=n; k++)
+ {
+ printf("Hello world!\n");
+ }
+ getch();
+ }
