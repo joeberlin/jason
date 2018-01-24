6b91bbd54bb0c4fc7b1e7885c287fbdd6a51a54b
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-24 16:40:24 +0530

    Create sumoftheinteger.c

---
+++ sumoftheinteger.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x, y, z = 0;
+printf("Enter a positive integer: ");
+scanf("%d",&y);
+for(x=1; a <= y; ++x)
+{
+ z+= x;  
+ }
+printf("Sum = %d",c);
+getch();
+ }   
