11b7a48e0b36b87d2b3eaea5267f8020d746482b
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-31 16:38:16 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,fact;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+{
+printf("%d!=%d",n,fact);
+}
+getch();
+}
+
