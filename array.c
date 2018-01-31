372a485d3dbd9e994646c1460d1e23fb5176ab80
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-01-31 16:55:11 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void  main()
+{
+int n, z, small, array[20];
+printf(" How many Integer numbers : ");
+scanf("%d", &n);
+z=n;
+if(n>0)
+{
+printf("\n Enter the numbers one by one : ");
+for(;n>=1;n--)
+scanf("%d", &array[n]);
+small = array[1];
+n=z;
+for(;n>=1; n--){
+if(small > array[n])
+small = array[n];
+}
+}
+printf("\n The smallest of %d numbers is %d", z, small);
+getch();   
