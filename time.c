610b0fbb35381a06107f908d18831ce1306599ff
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-07 15:41:41 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,m1,m2,p,sub;
+printf("\n Enter 1st time");
+scanf("%d%d",&a,&m1);
+printf("\n Enter 2nd time=");
+scanf("%d%d",&b,&m2);
+p=a-b;
+sub=m1-m2;
+printf("time:%d %d",p,sub");
+getch();
+}
