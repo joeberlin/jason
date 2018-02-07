f6496cc0d4c8dcac2a6e300412a2ba8c2d6a4939
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-07 15:34:22 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("Enter the character:");
+scanf("%c",&n);
+if(n>='0'&& n<='9')
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+getch();
+}
