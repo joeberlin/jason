7a613c27e209fbc5813daf3b12e1d3ea8cc1dad7
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-09 16:07:00 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+FILE *fp;
+char a[200];
+char ch;
+int linecount;
+linecount = 0;
+printf("enter the char");
+scanf("%c",&a);
+for(ch==0;ch<0;ch++)
+++linecount;
+{
+printf("Lines : %d \n", linecount);
+}
+getch();
+}
