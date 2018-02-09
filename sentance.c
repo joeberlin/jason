91299d763e4276f130566a3646737cc0fe3b17b0
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-09 15:39:25 +0530

    Create sentance.c

---
+++ sentance.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int i = 0; 
+int l = 0; 
+int f = 1;
+{
+printf("Enter the string \n");
+scanf(%c,a);
+for(i = 0;a[i]!='0';i++)
+{
+l = l + 1;
+}
+printf("The number of characters in the string are %d\n", l);
+}
+getch();
+}
