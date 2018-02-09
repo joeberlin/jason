0bf63fa3e27b1acbee9fe900397413e734e835ad
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-09 16:12:00 +0530

    Rename space.c to linespace.c

---
+++ linespace.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+FILE *fp;
+char a[200];
+char ch;
+int linespace;
+int count = 0;
+printf("enter the char");
+scanf("%c",&a);
+for(ch==0;ch<0;ch++)
+++linespace;
+++ count;
+{
+printf("Lines : %d \n", linespace);
+}
+getch();
+}
