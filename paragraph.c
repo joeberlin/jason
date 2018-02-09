09009800dd961344c7484a6c8ec4d84dda4c633f
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-09 16:17:08 +0530

    Create paragraph.c

---
+++ paragraph.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+FILE *fp;
+char a[200];
+char ch;
+char paragraphline;
+int count = 0;
+printf("enter the paragraph");
+scanf("%c",&a);
+for(ch==0;ch<0;ch++)
+++paragraphline;
+++ count;
+{
+printf("Lines : %d \n", paragraphline);
+}
+getch();
+}
