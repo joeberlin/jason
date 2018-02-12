8007b6dd8533fb53083f1971333516c9229bdae7
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-12 16:39:13 +0530

    Create kitems.c

---
+++ kitems.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,n;
+printf("\n enter the number of times:");
+scanf("%d",&n);
+for(s=0;s<n;s++)
+{
+printf("\n hai beni");
+}
+getch();
+}
