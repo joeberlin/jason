d5107e3808b34aeded62df3e2ed04f20dc3cf639
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-12 16:10:58 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+double firstNum, secondNum, temporary;
+printf("Enter first num: ");
+scanf("%lf", &firstNum);
+printf("Enter second num: ");
+scanf("%lf",&secondNum);
+temporary = firstNur;
+firstNum = secondNum;
+secondNumber = temporary;
+printf("\nAfter swapping");
+scanf("firstNum= %.2lf\n", firstNum);
+printf("After swapping"); 
+scanf("secondNum= %.2lf", secondNum);
+return 0;
+}
+
