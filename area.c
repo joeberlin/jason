8d17fbe9e3de686d85a5ef59a7455ece8a49ab41
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-12 16:19:02 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>.
+void area();  
+void main()
+{
+area();
+}
+void area()
+{
+float area _circle;
+float rad;
+printf("\nEnter the radius : ");
+scanf("%f",&rad);
+area_circle = 3.14 * rad * rad ;
+printf("Area of Circle = %f",area_circle);
+getch();
+}
