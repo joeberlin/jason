b88381bffcf2afc51492559b97061f897c49745c
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-19 16:40:44 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("enter the number a");
+scanf("%d",&a);
+printf("enter the number b");
+scanf("%d",&b);
+a=a+b;
+printf("add the two numbe r%d",a);
+getch();
+}
