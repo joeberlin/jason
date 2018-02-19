6853de6879f9b68b4e7ff6cf07ec90482346af53
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-19 16:10:25 +0530

    Create two.c

---
+++ two.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str1[20],str2[20];
+printf("\n enter the two strings");
+scanf("%s%s",&str1,&str2);
+printf("%s%s",str1,str2);
+getch();
+}
