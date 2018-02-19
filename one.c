16b79f7bde42f2159af3fc9fdda3e95e9a01d13c
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-19 16:06:33 +0530

    Create one.c

---
+++ one.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number to print\n");
+scanf("%d",&n);
+if(n<=10)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
