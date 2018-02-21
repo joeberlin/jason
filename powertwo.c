954306d304811a085aaa821245b820257c3369c5
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-21 16:17:49 +0530

    Rename power.c to powertwo.c

---
+++ powertwo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+isPowerOfTwo(int n)
+{
+return (ceil(log2(n)) == floor(log2(n)));
+}
+void main()
+{
+isPowerOfTwo(31)? printf("Yes\n"): printf("No\n");
+isPowerOfTwo(64)? printf("Yes\n"): printf("No\n");
+getch();    
+}
