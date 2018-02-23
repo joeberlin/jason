7b77c4fb35ba8a626c2902d1d8d39e8fe1117b16
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-23 15:43:07 +0530

    Create same.c

---
+++ same.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+printf("print the all digits number:%d%d%d",a,b,c);
+getch();
+}
