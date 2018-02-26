3a4db442d5e9ce6e69e3661d47829071b17e1391
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-26 15:41:38 +0530

    Create protwo.c

---
+++ protwo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter the two numbers");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n the number is even");
+}
+else
+{
+printf("\n the number is odd");
+}
+getch();
+}
