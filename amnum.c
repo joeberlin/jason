3fe931fc2adbdee0cbbf53c07142bdff165d14d0
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-26 16:45:34 +0530

    Create amnum.c

---
+++ amnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n enter the  10 numbers");
+scanf("%d",&a);
+for(a=0;a>=0;a++)
+{
+printf("among the number %d",a);
+}
+getch();
+}
