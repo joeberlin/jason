961f6d30e59768e6504b28e9801a9d0438b60e93
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-26 16:03:56 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+int a;
+char str[30];
+printf("\n enter the number and the string");
+scanf("%d",&a);
+scanf("%c",&str[20]);
+if(str[30]==a)
+{
+printf("\n no");
+}
+else
+{
+printf("\n yes");
+}
+getch();
+}
+
