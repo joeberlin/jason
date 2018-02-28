735ed8ad0e81b19c40e59682b521109c38fc8a19
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-02-28 15:52:45 +0530

    Create stringnum.c

---
+++ stringnum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+char str1[30];
+clrscr();
+printf("enter the string");
+scanf("%c",&str1[i]);
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%c",str[i]);
+}
+getch();
+}
+
