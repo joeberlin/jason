f614fe432b1d83cff7f4509b5683c3cfb5e09f23
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-05 16:45:53 +0530

    Create vowels.cpp

---
+++ vowels.cpp
@@ -0,0 +1,38 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+char str[40];
+cout<<"enter the alphabets";
+cin>>str;
+}
+switch(vowels)
+{
+case 0:
+cout<<'a';
+break;
+case 1:
+cout<<'e';
+break;
+case 2:
+cout<<'i';
+break;
+case 3:
+cout<<'o';
+break;
+case 4:
+cout<<'u';
+break;
+defalt:
+cout<<"invalid";
+break;
+}
+if(vowels==str)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+getch();
+}
