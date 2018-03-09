acc522cd917ee47155ae066b21e69eb2863f7b70
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-09 15:55:57 +0530

    Create yes.cpp

---
+++ yes.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,s,m,i;
+printf("Enter the numbers:");
+scanf("%d%d",&num,&s);
+m=num*s;
+for(i=0;i<=n;i++)
+{
+if(m ==i*i)
+{
+printf("\n YES");
+}
+printf("\n NO");
+}
+getch();   
+}
