08ac61ff8c44bc6a52d38929cbe37d51dea8ee17
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-12 16:08:41 +0530

    Create dec.cpp

---
+++ dec.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+float l,b,a;
+clrscr();
+cout<<"enter the lenth";
+cin>>l;
+cout<<"enter the breadh";
+cin>>b;
+a=l*b;
+cout<<"the area is"<<a;
+getch();
+}
