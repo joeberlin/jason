debd5a9c67729a5abb66c2ae49c23b34506a2fa3
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-14 16:31:12 +0530

    Rename two.cpp to addtwo.cpp

---
+++ addtwo.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int k;
+int a,b,c,d;
+cout<<"enter the number";
+cin>>k;
+cout<<"enter the number";
+cin>>a>>b>>c>>d;
+k=a+b+c+d;
+cout<<"the number is"<<k;
+getch();
+}
