48ab0345d9b5cb3eb614fb9b451d311809480856
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-14 16:45:16 +0530

    Create kdigit.c

---
+++ kdigit.c
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+int reversDigits(int num)
+{
+int rev_num=0;
+while(num>0)
+{
+rev_num=rev_num*10+num%10;
+num=num/10;
+}
+return rev_num;
+}
+void main()
+{
+int num=34;
+printf("Reverse of no is reversDigits(num));
+getch();
+}
