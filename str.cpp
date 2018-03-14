47d4d55d636f353a98c3b8f5fb871fdfbb05b614
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-14 16:19:51 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+char k[100];
+int u,i;
+scanf("%s",k);
+u=strlen(k);
+for(i=0;i<n;i++)
+{
+if(k[i]>=48&&a[i]<=57)
+printf("%c",k[i]);
+}
+return 0;
+}
