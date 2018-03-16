b3cc822ff9f70f275e47f9ee85b2ff4a2276f818
joeberlin <30549549+joeberlin@users.noreply.github.com>
2018-03-16 16:15:39 +0530

    Rename com.cpp to prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int num,i,count=0;
+cout<<"\n Enter Number:";
+cin>>num;
+for(i=2;i<num;i++)
+{
+if(num%i==0)
+{
+count++;
+break;
+}
+}
+if(count==0)
+{
+cout<<"\n Prime Number";
+}
+else
+{
+cout<<" \n Not a Prime Number";
+}        
+getch();
+}
